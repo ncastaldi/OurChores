f4d17142369ca88b413103f4243a4cba10b2afd0
Nathan Castaldi <nathan@castaldi.dev>
2021-04-09 08:45:32 -0400

    added heroku scirpts

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(express.static("client/build"));
+
 mongoose
     .connect(process.env.MONGODB_URI || "mongodb://localhost/ourchores", {
         useNewUrlParser: true,
@@ -24,6 +26,12 @@ mongoose
 app.get('/', function (req, res) {
     res.send('Hello World!')
 });
+
+if (process.env.PRODUCTION_URL) {
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "client/build/index.html"));
+    });
+};
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
