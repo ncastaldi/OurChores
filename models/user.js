79bdd5e5d8f1def43f9d1d2154a786867d5ece4f
Nathan Castaldi <nathan@castaldi.dev>
2021-04-09 12:28:19 -0400

    created basic user schema; listed in model index

---
+++ models/user.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const models = require(".");
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
