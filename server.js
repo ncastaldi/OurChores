e18a89c24f49a697945bf75a536d0642737a91f0
Nathan Castaldi <nathan@castaldi.dev>
2021-04-09 08:32:23 -0400

    established connection to DB; tested working

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+mongoose
+    .connect(process.env.MONGODB_URI || "mongodb://localhost/ourchores", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true,
+    })
+    .then(() => {
+        console.log("Successfully connected to MongoDB");
+    })
+    .catch((err) => {
+        console.log("Error connecting to MongoDB", err);
+    });
 
 app.get('/', function (req, res) {
     res.send('Hello World!')
