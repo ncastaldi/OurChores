cda84abd3f7900f0e5329532a5c1edfb923d686c
Nathan Castaldi <nathan@castaldi.dev>
2021-04-10 10:21:28 -0400

    created basic room model

---
+++ models/room.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const roomSchema = new Schema({
+    name: {
+        type: String,
+        trim: true,
+        required: true,
+    },
+});
+
+const Chore = mongoose.model("Room", roomSchema);
+
+module.exports = Room;
