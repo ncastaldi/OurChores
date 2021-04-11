d8a16f5593db2fa7e8b950c5d61b49c6f620012b
Nathan Castaldi <nathan@castaldi.dev>
2021-04-11 10:01:57 -0400

    listed chore routes in index

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const userRoutes = require("./user");
+const userRoutes = require("./chore");
 
 router.use("/user", userRoutes);
+router.use("/chore", choreRoutes);
 
 module.exports = router;
